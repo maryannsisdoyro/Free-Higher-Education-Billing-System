a67542b4c35b6f9f744fb3d6638e1acd8e86ab01
Rogine Bawiga <bawigarogine002@gmail.com>
2024-06-29 02:49:53 +0800

    first commit

---
+++ database/sfps_db.sql
@@ -0,0 +1,287 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 28, 2024 at 08:46 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `sfps_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `courses`
+--
+
+CREATE TABLE `courses` (
+  `id` int(30) NOT NULL,
+  `course` varchar(100) NOT NULL,
+  `description` text NOT NULL,
+  `level` varchar(150) NOT NULL,
+  `laboratory` int(11) NOT NULL,
+  `computer` int(11) NOT NULL,
+  `academic` int(11) NOT NULL,
+  `academic_nstp` int(11) NOT NULL,
+  `total_amount` float NOT NULL,
+  `date_created` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `courses`
+--
+
+INSERT INTO `courses` (`id`, `course`, `description`, `level`, `laboratory`, `computer`, `academic`, `academic_nstp`, `total_amount`, `date_created`) VALUES
+(2, 'Bachelor of Science in Information Technology(BSIT)', 'dsfdsfsd', '3rd ', 0, 0, 0, 0, 3125.02, '2024-06-28 13:05:29');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `fees`
+--
+
+CREATE TABLE `fees` (
+  `id` int(30) NOT NULL,
+  `course_id` int(30) NOT NULL,
+  `description` varchar(200) NOT NULL,
+  `amount` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `fees`
+--
+
+INSERT INTO `fees` (`id`, `course_id`, `description`, `amount`) VALUES
+(4, 2, 'Tuition Fee based on enrolled academic units(credit and none-credit courses) ', 1375.02),
+(17, 2, 'NSTP based on enrolled academic units (credit and non-credit courses)', 0),
+(18, 2, 'Athletic Fees', 150),
+(19, 2, 'Computer Fees', 100),
+(20, 2, 'Cultural Fees', 200),
+(21, 2, 'Development Fees', 250),
+(22, 2, 'Entrance/Admission Fees*', 200),
+(23, 2, 'Guidance Fees', 100),
+(24, 2, 'Handbook Fees', 0),
+(25, 2, 'Laboratory Fees', 0),
+(26, 2, 'Library Fee', 150),
+(27, 2, 'Medical and Dental Fees', 300),
+(28, 2, 'Registration Fees', 300),
+(29, 2, 'School ID Fees', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payments`
+--
+
+CREATE TABLE `payments` (
+  `id` int(30) NOT NULL,
+  `ef_id` int(30) NOT NULL,
+  `amount` float NOT NULL,
+  `remarks` text NOT NULL,
+  `date_created` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student`
+--
+
+CREATE TABLE `student` (
+  `id` int(30) NOT NULL,
+  `id_no` varchar(100) NOT NULL,
+  `name` text NOT NULL,
+  `fname` text NOT NULL,
+  `lname` text NOT NULL,
+  `mname` text DEFAULT NULL,
+  `contact` varchar(100) NOT NULL,
+  `address` text NOT NULL,
+  `email` varchar(200) NOT NULL,
+  `date_created` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `student`
+--
+
+INSERT INTO `student` (`id`, `id_no`, `name`, `fname`, `lname`, `mname`, `contact`, `address`, `email`, `date_created`) VALUES
+(1, '06232014', 'John Smith', '', '', '', '+18456-5455-55', 'Sample Address', 'jsmith@sample.com', '2020-10-31 11:24:42'),
+(2, '1415', 'George Wilson', '', '', '', '+18456-5455-55', 'Sample Address', 'gwilson@sample.com', '2020-10-31 11:29:38'),
+(3, '32313', 'fsff sdfsfs sfsdfs', 'fsff', 'sfsdfs', 'sdfsfs', '09094511207', 'dsfdsfs', 'bawiga@gmail.com', '2024-06-28 13:02:46');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student_ef_list`
+--
+
+CREATE TABLE `student_ef_list` (
+  `id` int(30) NOT NULL,
+  `student_id` int(30) NOT NULL,
+  `ef_no` varchar(200) NOT NULL,
+  `course_id` int(30) NOT NULL,
+  `total_fee` float NOT NULL,
+  `date_created` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `student_ef_list`
+--
+
+INSERT INTO `student_ef_list` (`id`, `student_id`, `ef_no`, `course_id`, `total_fee`, `date_created`) VALUES
+(1, 2, '2020-654278', 1, 4500, '2020-10-31 12:04:18'),
+(2, 1, '2020-65427823', 1, 4500, '2020-10-31 13:12:13'),
+(3, 3, '23232', 2, 1597.02, '2024-06-28 13:07:21');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `system_settings`
+--
+
+CREATE TABLE `system_settings` (
+  `id` int(30) NOT NULL,
+  `name` text NOT NULL,
+  `email` varchar(200) NOT NULL,
+  `contact` varchar(20) NOT NULL,
+  `cover_img` text NOT NULL,
+  `about_content` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `system_settings`
+--
+
+INSERT INTO `system_settings` (`id`, `name`, `email`, `contact`, `cover_img`, `about_content`) VALUES
+(1, 'School Fees Payment System', '', '', '', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(30) NOT NULL,
+  `name` text NOT NULL,
+  `username` varchar(200) NOT NULL,
+  `password` text NOT NULL,
+  `type` tinyint(1) NOT NULL DEFAULT 3 COMMENT '1=Admin,2=Staff'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `username`, `password`, `type`) VALUES
+(1, 'admin', 'admin', '0192023a7bbd73250516f069df18b500', 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `courses`
+--
+ALTER TABLE `courses`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `fees`
+--
+ALTER TABLE `fees`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `payments`
+--
+ALTER TABLE `payments`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `student`
+--
+ALTER TABLE `student`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `student_ef_list`
+--
+ALTER TABLE `student_ef_list`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `system_settings`
+--
+ALTER TABLE `system_settings`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `courses`
+--
+ALTER TABLE `courses`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `fees`
+--
+ALTER TABLE `fees`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+
+--
+-- AUTO_INCREMENT for table `payments`
+--
+ALTER TABLE `payments`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `student`
+--
+ALTER TABLE `student`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `student_ef_list`
+--
+ALTER TABLE `student_ef_list`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `system_settings`
+--
+ALTER TABLE `system_settings`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ receipt.php
@@ -0,0 +1,147 @@
+<?php
+include 'db_connect.php';
+$fees = $conn->query("SELECT ef.*,s.name as sname, s.lname, s.mname, s.fname, s.email, s.contact,s.id_no,concat(c.course,' - ',c.level) as `class`, c.course, c.level, c.laboratory, c.computer, c.academic, c.academic_nstp FROM student_ef_list ef inner join student s on s.id = ef.student_id inner join courses c on c.id = ef.course_id  where ef.id = {$_GET['ef_id']}");
+foreach ($fees->fetch_array() as $k => $v) {
+	$$k = $v;
+}
+$payments = $conn->query("SELECT * FROM payments where ef_id = $id ");
+$pay_arr = array();
+while ($row = $payments->fetch_array()) {
+	$pay_arr[$row['id']] = $row;
+}
+?>
+
+<style>
+	.flex {
+		display: inline-flex;
+		width: 100%;
+	}
+
+	.w-50 {
+		width: 50%;
+	}
+
+	.text-center {
+		text-align: center;
+	}
+
+	.text-right {
+		text-align: right;
+	}
+
+	table.wborder {
+		width: 100%;
+		border-collapse: collapse;
+	}
+
+	table.wborder>tbody>tr,
+	table.wborder>tbody>tr>td {
+		border: 1px solid;
+	}
+
+	p {
+		margin: unset;
+	}
+
+	tr td{
+		text-align: right;
+	}
+
+	@media print {
+    table, thead, th, tr, td {
+        border: 1px solid #000 !important;
+        border-collapse: collapse !important;
+		text-align: right;
+    }
+}
+
+</style>
+<div class="container-fluid">
+
+	<div class="table-responsive">
+		<table class="wborder" style="border: 1px solid #000; border-collapse: collapse;">
+			<tr>
+				<td colspan="29" class="text-center">
+					<p class="mt-3">Republic of the Philippines</p>
+					<p><i>Madridejos Community College</i></p>
+					<p><i>Bunakan, Madridejos, Cebu</i></p>
+
+					<h6 class="my-4"><b>FREE HIGHER EDUCATION BILLING DETAILS</b></h6>
+
+				</td>
+			</tr>
+			<tr>
+				<td width="50%">
+					<p><b>TUITION AND OTHER SCHOOL FEES(Based on Section 7, Rule II of the RA 10931)</b></p>
+					<hr>
+					<table class="table table-bordered" width="100%">
+						
+						<thead>
+							<th style="min-width: 200px !important;">Sequence Number</th>
+							<th style="min-width: 200px !important;">Student Number</th>
+							<th style="min-width: 200px !important;">Learner's Reference Number</th>
+							<th style="min-width: 200px !important;">Last Name</th>
+							<th style="min-width: 200px !important;">Given's Name</th>
+							<th style="min-width: 200px !important;">Middle Name</th>
+							<th style="min-width: 200px !important;">Degree Program</th>
+							<th>Year Level</th>
+							<th style="min-width: 200px !important;">E-mail Address</th>
+							<th style="min-width: 200px !important;">Phone Number</th>
+							<th style="min-width: 200px !important;">Laboratory Units/subject</th>
+							<th style="min-width: 200px !important;">Computer Lab Units/subject</th>
+							<th style="min-width: 200px !important;">Academic Units Enrolled(credit and non-credit courses)</th>
+							<th style="min-width: 200px !important;">Academic Units of NSTP Enrolled(credit and non-credit courses)</th>
+							<?php
+							$cfees = $conn->query("SELECT * FROM fees where course_id = $course_id");
+							$ftotal = 0;
+							while ($row = $cfees->fetch_assoc()) {
+								$ftotal += $row['amount'];
+							?>
+
+								<th style="min-width: 200px !important;"><b><?php echo $row['description'] ?></b></th>
+								
+
+							<?php
+							}
+							?>
+							<th>Total</th>
+						</thead>
+
+						<tr>
+							<td><?= '00' . $student_id ?></td>
+							<td><?= $ef_no ?></td>
+							<td></td>
+							<td><?= ucfirst($lname) ?></td>
+							<td><?= ucfirst($fname) ?></td>
+							<td><?= ucfirst($mname) ?></td>
+							<td><?= $course ?></td>
+							<td><?= $level ?></td>
+							<td><?= $email ?></td>
+							<td><?= $contact ?></td>
+							<td><?= $laboratory ?></td>
+							<td><?= $computer ?></td>
+							<td><?= $academic ?></td>
+							<td><?= $academic_nstp ?></td>
+							<?php
+							$cfees = $conn->query("SELECT * FROM fees where course_id = $course_id");
+							$ftotal = 0;
+							while ($row = $cfees->fetch_assoc()) {
+								$ftotal += $row['amount'];
+							?>
+
+								
+								<td class='text-right'><b><?php echo number_format($row['amount'], 2) ?></b></td>
+
+							<?php
+							}
+							?>
+							<th class="text-right"><b><?php echo number_format($ftotal, 2) ?></b></th>
+						</tr>
+						
+					</table>
+				</td>
+			
+			</tr>
+		</table>
+	</div>
+</div>
