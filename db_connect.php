d434728928d2be0851c121b44e02b0602dc86387
maryannsisdoyro <sisdoyromaryannlawan20@gmail.com>
2024-07-31 17:40:49 +0800

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,4 +1,4 @@
 <?php 
 
-$conn= new mysqli('localhost','u510162695_fhebilling','1Fhebilling','u510162695_fhebilling')or die("Could not connect to mysql".mysqli_error($con));
+$conn= new mysqli('127.0.0.1','u510162695_fhebilling','1Fhebilling','u510162695_fhebilling')or die("Could not connect to mysql".mysqli_error($con));
 // $conn= new mysqli('localhost','root','','sfps_db')or die("Could not connect to mysql".mysqli_error($con));
