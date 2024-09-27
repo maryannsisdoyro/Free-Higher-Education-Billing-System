2bda16c0119f6f12a221c9693fa0b7eb70a40b49
maryannsisdoyro <sisdoyromaryannlawan20@gmail.com>
2024-09-26 20:43:31 -0700

    Update index.php

--- enrol/index.php
+++ enrol/index.php
@@ -6,7 +6,6 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="icon" type="image/x-icon" href="assets/logo.png">
     <title>Student Enrollment</title>
  	
     <!-- Bootstrap CSS -->
