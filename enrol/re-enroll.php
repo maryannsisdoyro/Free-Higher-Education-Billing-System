951dd8303a8c56387072f19bfc522b79d7764d9f
maryannsisdoyro <sisdoyromaryannlawan20@gmail.com>
2024-09-28 21:29:51 -0700

    Update re-enroll.php

--- enrol/re-enroll.php
+++ enrol/re-enroll.php
@@ -133,7 +133,7 @@
                               <td>
           
                                       <div class="dropdown">
-                                            <button type='button' class="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown">Select to Enroll</button>
+                                            <button type='button' class="btn btn-primary dropdown-toggle" data-toggle="dropdown">Select to Enroll</button>
                                             <ul class="dropdown-menu">
                                                 <li class="dropdown-item">
                                                    <button type="submit" name="submit" class="btn btn-primary px-5">Enroll</button>
