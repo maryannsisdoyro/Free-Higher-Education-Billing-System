d7294d0b5ac9d79f818b0a9157ef146c91ab919f
maryannsisdoyro <sisdoyromaryannlawan20@gmail.com>
2024-11-08 23:10:49 +0800

    commit

--- navbar.php
+++ navbar.php
@@ -22,7 +22,7 @@
 
 				<div class="mx-2 text-white">Report</div>
 
-				<!-- <a href="index.php?page=fees" class="nav-item nav-fees"><span class='icon-field mr-2'><i class="fa fa-money-check "></i></span> Student Fees</a> -->
+				<a href="index.php?page=fees" class="nav-item nav-fees"><span class='icon-field mr-2'><i class="fa fa-money-check "></i></span> Student Fees</a>
 				<a href="index.php?page=payments" class="nav-item nav-payments"><span class='icon-field mr-2'><i class="fa fa-receipt "></i></span> Payments</a>
 
 				
@@ -35,7 +35,7 @@
 
 				<?php if($_SESSION['login_type'] == 1): ?>
 				<a href="index.php?page=users" class="nav-item nav-users"><span class='icon-field mr-2'><i class="fa fa-users "></i></span> Users</a>
-				<a href="index.php?page=downloadable_form" class="nav-item nav-site_settings"><span class='icon-field'><i class="fa fa-download"></i></span> Downloadable Form</a>
+				<!-- <a href="index.php?page=site_settings" class="nav-item nav-site_settings"><span class='icon-field'><i class="fa fa-cogs"></i></span> System Settings</a> -->
 			<?php endif; ?>
 			<a href="./enrol/home.php?page=settings" class="nav-item nav-settings"><span class='icon-field mr-2'><i class="fa fa-wrench"></i></span> Settings</a>
 		</div>
