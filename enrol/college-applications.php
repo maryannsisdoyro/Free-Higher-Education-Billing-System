54bb98f579d1bbbdad8cf408bffe37e6852e2bda
maryannsisdoyro <sisdoyromaryannlawan20@gmail.com>
2024-11-16 14:46:12 +0800

    commit

--- enrol/college-applications.php
+++ enrol/college-applications.php
@@ -44,7 +44,39 @@ $conn->close();
     <title>Official Enrollment List</title>
     <link rel="icon" type="image/x-icon" href="../assets/logo.png">	
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+   <!-- Bootstrap CSS -->
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="../assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../assets/vendor/icofont/icofont.min.css" rel="stylesheet">
+    <link href="../assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="../assets/vendor/venobox/venobox.css" rel="stylesheet">
+    <link href="../assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+    <link href="../assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+    <link href="../assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+    <link href="../assets/vendor/bootstrap-datepicker/css/bootstrap-datepicker.min.css" rel="stylesheet">
+    <link href="../assets/DataTables/datatables.min.css" rel="stylesheet">
+    <link href="../assets/css/jquery.datetimepicker.min.css" rel="stylesheet">
+    <link href="../assets/css/select2.min.css" rel="stylesheet">
+
+
+    <!-- Template Main CSS File -->
+    <link href="../assets/css/style.css" rel="stylesheet">
+    <link type="text/css" rel="stylesheet" href="../assets/css/jquery-te-1.4.0.css">
+
+    <script src="../assets/vendor/jquery/jquery.min.js"></script>
+    <script src="../assets/DataTables/datatables.min.js"></script>
+    <script src="../assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="../assets/vendor/jquery.easing/jquery.easing.min.js"></script>
+    <script src="../assets/vendor/php-email-form/validate.js"></script>
+    <script src="../assets/vendor/venobox/venobox.min.js"></script>
+    <script src="../assets/vendor/waypoints/jquery.waypoints.min.js"></script>
+    <script src="../assets/vendor/counterup/counterup.min.js"></script>
+    <script src="../assets/vendor/owl.carousel/owl.carousel.min.js"></script>
+    <script src="../assets/vendor/bootstrap-datepicker/js/bootstrap-datepicker.min.js"></script>
+    <script type="text/javascript" src="../assets/js/select2.min.js"></script>
+    <script type="text/javascript" src="../assets/js/jquery.datetimepicker.full.min.js"></script>
+    <script type="text/javascript" src="../assets/font-awesome/js/all.min.js"></script>
+    <script type="text/javascript" src="../assets/js/jquery-te-1.4.0.min.js" charset="utf-8"></script>
     <style>
         body {
             background-color: #f8f9fa;
