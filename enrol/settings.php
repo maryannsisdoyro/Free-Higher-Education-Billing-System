96ec838482b55cc4a0946f938b61fcf4e2e01c79
maryannsisdoyro <sisdoyromaryannlawan20@gmail.com>
2024-11-16 14:37:54 +0800

    commit

--- enrol/settings.php
+++ enrol/settings.php
@@ -11,6 +11,7 @@ $academic_query = $conn->query("SELECT * FROM academic");
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Student List</title>
+    <link rel="icon" type="image/x-icon" href="../assets/logo.png">	
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
     <link href="sweetalerts/sweetalert2.min.css" rel="stylesheet">
