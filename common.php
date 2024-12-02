c7b3b47d09ed6cba8671d75ce2d7bd187a1cbf94
maryannsisdoyro <sisdoyromaryannlawan20@gmail.com>
2024-12-02 22:20:39 +0800

    commit

--- common.php
+++ common.php
@@ -1,11 +1,7 @@
 <?php
-// - Set the X-Frame-Options header for clickjacking protection
+ob_start(); // Start output buffering to prevent issues with sending headers
 header('X-Frame-Options: SAMEORIGIN');
-
-// - Set the X-Content-Type-Options header to prevent MIME sniffing
 header('X-Content-Type-Options: nosniff');
-
-// - Set the Content Security Policy header with multiple directives
-header("Content-Security-Policy: default-src 'self'; script-src 'self';");
+header("Content-Security-Policy: default-src 'self'; script-src 'self'; object-src 'self'; base-uri 'self';");
 exit;
 ?>
