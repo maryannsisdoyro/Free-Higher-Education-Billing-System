73ba60b1a4ce36251a3eaa1627ba86ce70647bef
maryannsisdoyro <sisdoyromaryannlawan20@gmail.com>
2024-12-02 22:04:46 +0800

    commit

--- common.php
+++ common.php
@@ -3,6 +3,6 @@
 header('X-Frame-Options: SAMEORIGIN');
 
 // - Set the Content Security Policy header with multiple directives
-header("Content-Security-Policy: default-src 'self'; script-src 'self'; object-src 'self'; base-uri 'self';");
+header("Content-Security-Policy: default-src 'self'; script-src 'self';");
 exit;
 ?>
