70630fc57098fd43c9e7944820f6b6a538256d6e
maryannsisdoyro <sisdoyromaryannlawan20@gmail.com>
2024-12-02 22:29:35 +0800

    commit

--- common.php
+++ common.php
@@ -2,6 +2,6 @@
 ob_start(); // Start output buffering to prevent issues with sending headers
 header('X-Frame-Options: SAMEORIGIN');
 header('X-Content-Type-Options: nosniff');
-header("Content-Security-Policy: default-src 'self'; script-src 'self'; object-src 'self'; base-uri 'self';");
+header("Content-Security-Policy: default-src 'self'; script-src 'self'; object-src 'self'; base-uri 'self'; upgrade-insecure-requests;");
 exit;
 ?>
