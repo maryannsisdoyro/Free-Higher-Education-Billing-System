05b5c4b9670a4e96908d0b53b3117bcb273a3439
maryannsisdoyro <sisdoyromaryannlawan20@gmail.com>
2024-12-02 21:57:37 +0800

    update common func

--- common.php
+++ common.php
@@ -1,3 +1,12 @@
 <?php
+// - Set the X-Frame-Options header for clickjacking protection
 header('X-Frame-Options: SAMEORIGIN');
+
+// - Set the Content Security Policy header with multiple directives
+header("Content-Security-Policy: 
+    default-src 'self'; 
+    script-src 'self'; 
+    object-src 'self'; 
+    base-uri 'self';");
+
 ?>
