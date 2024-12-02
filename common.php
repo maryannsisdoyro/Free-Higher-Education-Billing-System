ee256a0c7c9aa37795f131c2ad8028a3f3b5ce4e
maryannsisdoyro <sisdoyromaryannlawan20@gmail.com>
2024-12-02 22:57:28 +0800

    remove test

--- common.php
+++ common.php
@@ -1,7 +1,7 @@
 <?php
-ob_start(); // Start output buffering to prevent issues with sending headers
+// ob_start(); // Start output buffering to prevent issues with sending headers
 header('X-Frame-Options: SAMEORIGIN');
 header('X-Content-Type-Options: nosniff');
 header("Content-Security-Policy: default-src 'self'; script-src 'self'; object-src 'self'; base-uri 'self'; upgrade-insecure-requests;");
-exit;
+// exit;
 ?>
