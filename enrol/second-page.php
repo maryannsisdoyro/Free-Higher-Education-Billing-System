1a498aa54bc17ec11d4eeb6c8762d1845db7d4e0
maryannsisdoyro <sisdoyromaryannlawan20@gmail.com>
2024-12-04 05:22:08 +0800

    commit

--- enrol/second-page.php
+++ enrol/second-page.php
@@ -940,8 +940,8 @@ if ($row) {
                 </div>
             </div>
 
-            <div class="w-100 text-end">
-            <a href="last-page.php?application_no=<?= $id ?>" class="btn btn-danger my-3 mx-auto d-block" style="width: 100px;" onclick="document.getElementById('final_process').classList.remove('d-none'); this.classList.add('d-none')" id="to_assessment">Next</a>
+            <div class="w-100 text-end d-flex align-items-center gap-2 my-3">
+            <a href="last-page.php?application_no=<?= $id ?>" class="btn btn-danger d-block" style="width: 100px;" onclick="document.getElementById('final_process').classList.remove('d-none'); this.classList.add('d-none')" id="to_assessment">Next</a>
             <a href="../index.php?page=college-application" class="btn btn-secondary" style="width: 100px;">Cancel</a>
             </div>
             <!-- Include jQuery and DataTables JS files -->
