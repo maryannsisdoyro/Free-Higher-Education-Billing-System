3b4f439828993d603a6dd83cd3ec34470cf5c20e
maryannsisdoyro <sisdoyromaryannlawan20@gmail.com>
2024-12-04 06:05:27 +0800

    commit

--- enrol/final-cor.php
+++ enrol/final-cor.php
@@ -1526,7 +1526,7 @@ $row['course'] = $row['course'] == 'BS-HM' ? 'BSHM' : $row['course'];
 				</tr>
 				<?php
                 $row['course'] = $row['course'] == 'BS-HM' ? 'BSHM' : $row['course'];
-                echo $row['course'];
+                // echo $row['course'];
 				$get_course = $conn->query("SELECT * FROM courses WHERE department = '".$row["course"]."' AND level = '". $y_level ."' AND semester = '". $semester ."' ");
                 
               
