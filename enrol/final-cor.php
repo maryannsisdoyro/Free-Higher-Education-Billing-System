b1946d09c1e1e7b47b368acdcf8a6355927ee691
maryannsisdoyro <sisdoyromaryannlawan20@gmail.com>
2024-12-04 06:12:28 +0800

    commit

--- enrol/final-cor.php
+++ enrol/final-cor.php
@@ -61,6 +61,7 @@ $all_course = [
     'BSED' => 'Bachelor of Secondary Education',
     'BSIT' => 'Bachelor of Science in Information Technology',
     'BSHM' => 'Bachelor of Science in Hotel Management',
+    'BS-HM' => 'Bachelor of Science in Hotel Management',
     'BSBA' => 'Bachelor of Science in Business Administration'
 ];
 
