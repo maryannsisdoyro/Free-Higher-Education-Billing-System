1701865e9d98c5052fca8260d5527c2df16e709d
maryannsisdoyro <sisdoyromaryannlawan20@gmail.com>
2024-12-04 05:02:18 +0800

    commit

--- payments_report.php
+++ payments_report.php
@@ -160,7 +160,7 @@
                                                 <?php
                                                   foreach ($data as $row1) {
                                              
-                                                    $cfees = $conn->query("SELECT * FROM student_individual_fees where enroll_id = '". $row1['id'] ."' ORDER BY id DESC LIMIT 1");
+                                                    $cfees = $conn->query("SELECT * FROM student_individual_fees where enroll_id = '". $row1['id'] ."' ORDER BY id DESC");
                                                     $ftotal = 0;
                                                     while ($row2 = $cfees->fetch_assoc()) {
                                                     ?>
