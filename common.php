2e92c8955a077844ec0741814892f1f9e616302d
maryannsisdoyro <sisdoyromaryannlawan20@gmail.com>
2024-12-03 09:01:36 +0800

    commit

--- common.php
+++ common.php
@@ -1,5 +1,9 @@
 <?php
 // ob_start(); // Start output buffering to prevent issues with sending headers
+// Ensure the site is served over HTTPS
+if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on') {
+    header('Strict-Transport-Security: max-age=31536000; includeSubDomains; preload');
+}
 header('X-Frame-Options: SAMEORIGIN');
 header('X-Content-Type-Options: nosniff');
 header("Content-Security-Policy: default-src 'self'; script-src 'self'; object-src 'self'; base-uri 'self'; upgrade-insecure-requests;");
