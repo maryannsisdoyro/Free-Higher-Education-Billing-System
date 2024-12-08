15534b33381a45524415feaacb1e3e826bbb86f2
maryannsisdoyro <sisdoyromaryannlawan20@gmail.com>
2024-12-08 11:15:06 +0800

    test

--- db_connect.php
+++ db_connect.php
@@ -2,3 +2,5 @@
 
 $conn= new mysqli('127.0.0.1','u510162695_fhebilling','1Fhebilling','u510162695_fhebilling')or die("Could not connect to mysql".mysqli_error($con));
 // $conn= new mysqli('localhost','root','','fhe_billing')or die("Could not connect to mysql".mysqli_error($con));
+
+// - docker test - $conn= new mysqli('mysql','testuser','testpassword','testdb')or die("Could not connect to mysql".mysqli_error($con));
