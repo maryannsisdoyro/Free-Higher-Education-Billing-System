4efa30f86bd0484c0d6a12709d69748cf7d94a36
maryannsisdoyro <sisdoyromaryannlawan20@gmail.com>
2024-12-09 07:42:35 +0800

    commit

--- enrol/last-page.php
+++ enrol/last-page.php
@@ -751,7 +751,7 @@ $ftotal  , 2) ?></b></td>
 
 <div class="w-100 d-flex align-items-center my-3 justify-content-end container" style="column-gap: 20px; padding-right: ;">
     <a href="student-cor.php?application_no=<?= $id ?>" class="btn btn-secondary" style="width: 100px;">Previous</a>
-<a href="" id="submit_btn" class="btn btn-danger d-block" style="width: 100px;">Submit</a>
+<a href="#" id="submit_btn" class="btn btn-danger d-block" style="width: 100px;">Submit</a>
 </div>
 
 <script>
