bd880d74564e7ef47177592bf6426deaeba8baf4
maryannsisdoyro <sisdoyromaryannlawan20@gmail.com>
2024-12-13 05:29:04 +0800

    commit

--- enrol/final-cor.php
+++ enrol/final-cor.php
@@ -1411,6 +1411,7 @@ $row['course'] = $row['course'] == 'BS-HM' ? 'BSHM' : $row['course'];
 					<td colspan="3">Total</td>
 				</tr>
 				<?php
+                echo $row['enroll_status'];
                 if ($row['enroll_status'] != 'shiftee' || $row['enroll_status'] != 'SHIFTEE') {
                     $row['course'] = $row['course'] == 'BS-HM' ? 'BSHM' : $row['course'];
                     $get_course = $conn->query("SELECT * FROM courses WHERE department = '".$row["course"]."' AND semester = '". $semester ."' ");
