680c11ce507b77bba01534e941cec8461ea59829
maryannsisdoyro <sisdoyromaryannlawan20@gmail.com>
2024-12-14 05:22:39 +0800

    commit

--- enrol/second-page.php
+++ enrol/second-page.php
@@ -474,12 +474,7 @@ if ($row) {
                 <?php 
                     if (isset($_POST['time'])) {
                         foreach ($_POST as $key => $value) {
-                            echo $key . "\n" . $value;
-                            if ($value != NULL) {
-                                $_SESSION['STUDENT_SUBJECT'][] = [
-                                    $key => $value
-                                ];
-                            }
+                            var_dump($_POST);
                         }
                     }
                 ?>
