6a1956c6495710ad09fcd9d94b148100c91288cb
maryannsisdoyro <sisdoyromaryannlawan20@gmail.com>
2024-12-13 16:57:39 +0800

    Update login.php

--- login.php
+++ login.php
@@ -111,27 +111,7 @@ ob_end_flush();
                   <a href="forgot-password.php">Forgot Password</a>
                   <button type="submit" class="btn-sm btn-block btn-wave col-md-4 btn-danger" id="login-button" disabled>Login</button>
               </div>
-<form id="login-form" method="POST">
-<!--   <p id="error-message" style="color: red; display: none;">Invalid credentials. Attempts left: <span id="attempts-left"></span></p>
-  <div class="form-group">
-    <label for="username" class="control-label">Email</label>
-    <input type="text" id="username" name="email" class="form-control">
-  </div>
-  <div class="form-group">
-    <label for="password" class="control-label">Password</label>
-    <div style="position: relative;">
-      <input type="password" id="password" name="password" class="form-control my-2">
-      <i class="bx bx-show fs-4" style="cursor: pointer; position: absolute; top: 0; right: 0; margin: 12px 10px 0 0; font-size: 15px;" id="show-pass1"></i>
-    </div>
-  </div>
-  <div class="form-group">
-    <input type="checkbox" id="terms-checkbox">
-    <label for="terms-checkbox"> I agree to the <a href="javascript:void(0);" id="showTerms">Terms and Conditions</a></label>
-  </div>
-  <div class="d-flex justify-content-between">
-    <a href="forgot-password.php">Forgot Password</a>
-    <button type="submit" class="btn-sm btn-block btn-wave col-md-4 btn-danger" id="login-button">Login</button>
-  </div> -->
+		    
               <script>
                   document.getElementById('terms-checkbox').addEventListener('change', function() {
                       document.getElementById('login-button').disabled = !this.checked;
@@ -390,10 +370,11 @@ window._conf = function($msg='',$func='',$params = []){
 </form>
 
 
+
 <script>
   const maxAttempts = 3;
   let attempts = maxAttempts;
-  let lockDuration = 30; // Lock duration in seconds
+  let lockDuration = 180; // Lock duration in seconds
   let isLocked = false;
 
   const loginForm = document.getElementById('login-form');
@@ -446,13 +427,11 @@ window._conf = function($msg='',$func='',$params = []){
   function resetForm() {
     isLocked = false;
     attempts = maxAttempts;
-    lockDuration = 30;
+    lockDuration = 180;
     errorMessage.style.display = 'none';
     loginButton.disabled = false;
     attemptsLeft.textContent = attempts;
   }
-  });
 </script>
-
 
 </html>
