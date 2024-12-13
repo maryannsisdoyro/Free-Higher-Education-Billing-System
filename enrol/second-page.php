203e284f2c9008174a71741860eecff5906be3cf
maryannsisdoyro <sisdoyromaryannlawan20@gmail.com>
2024-12-14 04:58:19 +0800

    commit

--- enrol/second-page.php
+++ enrol/second-page.php
@@ -512,7 +512,7 @@ if ($row) {
                     <td class="text-center units-column"><?php echo htmlentities($stud_sub['units']); ?></td>
                     <td class="text-center"><?php echo htmlentities($stud_sub['room']); ?></td>
                     <td class="text-center ins-column"><?php echo htmlentities($stud_sub['inst']); ?></td>
-                    <td class="text-center"><a href="?remove=<?= htmlentities($stud_sub['id']) ?>">x</td>
+                    <td class="text-center"><a href="?application_no=<?= $_GET['application_no'] ?>&remove=<?= htmlentities($stud_sub['id']) ?>">x</td>
                 </tr>
             <?php endforeach; ?>
         </tbody>
