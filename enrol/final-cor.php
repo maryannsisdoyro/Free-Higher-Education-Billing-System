eb0b236afbd831d075985287aeb59e212cecd5aa
maryannsisdoyro <sisdoyromaryannlawan20@gmail.com>
2024-12-14 06:45:12 +0800

    commit

--- enrol/final-cor.php
+++ enrol/final-cor.php
@@ -1029,7 +1029,7 @@ $row['course'] = $row['course'] == 'BS-HM' ? 'BSHM' : $row['course'];
             // Assuming $conn is your database connection
             $totalUnits = 0;
             $cou = $row['course'] == 'BSHM' ? 'BS-HM' : $row['course'];
-            $query = mysqli_query($conn, "SELECT * FROM subject WHERE course = '".$cou."' AND sem = '".$row['semester']."' AND year = '". $row['year_level'] ."'");
+            $query = mysqli_query($conn, "SELECT * FROM subject_individual WHERE course = '".$cou."' AND sem = '".$row['semester']."' AND year = '". $row['year_level'] ."'");
 
             foreach ($query as $row) :
                 $totalUnits += $row['units'];
