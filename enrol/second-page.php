b7f66a01a36e2bbaa5ca6e695f73debee488165c
maryannsisdoyro <sisdoyromaryannlawan20@gmail.com>
2024-12-14 14:23:25 +0800

    commit

--- enrol/second-page.php
+++ enrol/second-page.php
@@ -593,7 +593,7 @@ if (isset($_GET['next'])) {
                     <td class="text-center units-column"><?php echo htmlentities($stud_sub['units']); ?></td>
                     <td class="text-center"><?php echo htmlentities($stud_sub['room']); ?></td>
                     <td class="text-center ins-column"><?php echo htmlentities($stud_sub['inst']); ?></td>
-                    <td class="text-center"><a class="text-danger mx-2 btn btn-danger" href="?application_no=<?= $_GET['application_no'] ?>&remove=<?= htmlentities($stud_sub['id']) ?>">x</td>
+                    <td class="text-center"><a class="text-danger mx-2 py-0 btn btn-outline-danger" href="?application_no=<?= $_GET['application_no'] ?>&remove=<?= htmlentities($stud_sub['id']) ?>">x</td>
                 </tr>
             <?php endforeach; ?>
         </tbody>
