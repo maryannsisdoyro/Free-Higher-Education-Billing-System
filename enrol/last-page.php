8e6da53936af7e0b0d7048b3b53e8cdfac0ecee4
maryannsisdoyro <sisdoyromaryannlawan20@gmail.com>
2024-12-14 14:59:15 +0800

    commit

--- enrol/last-page.php
+++ enrol/last-page.php
@@ -685,23 +685,23 @@ $row['course'] = $row['course'] == 'BS-HM' ? 'BSHM' : $row['course'];
 
         $subject_total = $totalUnits * $rate;
 
-        echo $subject_total;
+        // echo $subject_total;
 
         
 
-        while ($rows = $cfees->fetch_assoc()) {
-            $ftotal[] = $rows['amount'];
-            $array[] = $rows['amount'];
-            echo $rows['amount'];
+        while ($row = $cfees->fetch_assoc()) {
+            $ftotal[] = $row['amount'];
+            $array[] = $row['amount'];
+            echo $row['amount'];
             // var_dump($array);
         ?>
 
 
         <tr>
-            <td colspan="2"><?= $rows['description'] ?></td>
+            <td colspan="2"><?= $row['description'] ?></td>
             <td colspan="5" style="text-align: center;">
                 <?php
-                    if ($rows['description'] == $tuition_based || $rows['description'] == $tuition_based2) {
+                    if ($row['description'] == $tuition_based || $row['description'] == $tuition_based2) {
                        echo number_format($totalUnits);
                     }else{
                         echo '-';
@@ -710,7 +710,7 @@ $row['course'] = $row['course'] == 'BS-HM' ? 'BSHM' : $row['course'];
             </td>
             <td colspan="2" style="text-align: center;">
             <?php
-                    if ($rows['description'] == $tuition_based || $rows['description'] == $tuition_based2) {
+                    if ($row['description'] == $tuition_based || $row['description'] == $tuition_based2) {
                        echo $rate;
                     }else{
                         echo '-';
@@ -719,10 +719,10 @@ $row['course'] = $row['course'] == 'BS-HM' ? 'BSHM' : $row['course'];
             </td>
             <td colspan="3" style="text-align: center;">
             <?php
-                    if ($rows['description'] == $tuition_based && $total_units != null) {
+                    if ($row['description'] == $tuition_based && $total_units != null) {
                        echo $subject_total;
                     }else{
-                        echo $rows['amount'];
+                        echo $row['amount'];
                     }
                 ?>
             </td>
