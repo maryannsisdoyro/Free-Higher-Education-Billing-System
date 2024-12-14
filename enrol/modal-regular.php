8ebd0f3c4e72e4484f68429c566656a6758381a0
maryannsisdoyro <sisdoyromaryannlawan20@gmail.com>
2024-12-14 13:56:48 +0800

    commit

--- enrol/modal-regular.php
+++ enrol/modal-regular.php
@@ -220,29 +220,29 @@
             end_load()
             return false;
         }
-        let data = new FormData($(this)[0])
-        console.log(data['type_regular']);
-        
-        // $.ajax({
-        //     url:'../ajax.php?action=save_regular',
-        //     data: new FormData($(this)[0]),
-        //     cache: false,
-        //     contentType: false,
-        //     processData: false,
-        //     method: 'POST',
-        //     type: 'POST',
-        //     success:function(resp){
-        //         const result = JSON.parse(resp)
-        //         if(result.status==1){
+        $.ajax({
+            url:'../ajax.php?action=save_regular',
+            data: new FormData($(this)[0]),
+            cache: false,
+            contentType: false,
+            processData: false,
+            method: 'POST',
+            type: 'POST',
+            success:function(resp){
+                const result = JSON.parse(resp)
+                // if(result.status==1){
                    
-        //                    location.href = "student-cor.php?application_no=" + result.enroll_id
+                //            location.href = "student-cor.php?application_no=" + result.enroll_id
                        
-        //         }else if(resp == 2){
-        //         $('#msg').html('<div class="alert alert-danger mx-2">Course Name & Level already exist.</div>')
-        //         end_load()
-        //         }   
-        //     }
-        // })
+                // }else if(resp == 2){
+                // $('#msg').html('<div class="alert alert-danger mx-2">Course Name & Level already exist.</div>')
+                // end_load()
+                // } 
+                
+                console.log(result);
+                
+            }
+        })
     })
 
     $('.select2').select2({
