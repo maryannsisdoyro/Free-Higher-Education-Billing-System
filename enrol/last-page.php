22a7a5f7fcd896648be27ec85aa3832b8fd12379
maryannsisdoyro <sisdoyromaryannlawan20@gmail.com>
2024-12-14 15:12:57 +0800

    commit

--- enrol/last-page.php
+++ enrol/last-page.php
@@ -738,8 +738,6 @@ $ftotal  , 2) ?></b></td>
 </div>
 <input type="hidden" id="application_no" name="appli_no" value="<?= $id ?>">
 
-<?= $ftotal ?>
-
 <div class="w-100 d-flex align-items-center my-3 justify-content-end container" style="column-gap: 20px; padding-right: ;">
     <a href="student-cor.php?application_no=<?= $id ?>" class="btn btn-secondary" style="width: 100px;">Previous</a>
 <a href="#" id="submit_btn" class="btn btn-danger d-block" style="width: 100px;">Submit</a>
