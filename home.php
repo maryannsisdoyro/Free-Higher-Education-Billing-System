85dcba4a5e41f873fa552bbf7fbefe608d541c3f
maryannsisdoyro <sisdoyromaryannlawan20@gmail.com>
2024-12-20 05:23:38 +0800

    commit

--- home.php
+++ home.php
@@ -198,12 +198,22 @@ src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js">
                 </div>
             </div>
 
-            <div class="col-lg-12">
+            <div class="col-lg-6">
                 <div class="card">
                     <div class="card-body">
                         <div id="genderchart">
 
                         </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-6">
+                <div class="card">
+                    <div class="card-body">
+                        <canvas id="myChart">
+
+                        </canvas>
                     </div>
                 </div>
             </div>
