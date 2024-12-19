fb7ce3f0a480fe34447b0ac3475cb7a35e850221
maryannsisdoyro <sisdoyromaryannlawan20@gmail.com>
2024-12-20 05:41:41 +0800

    commit

--- system-backup.php
+++ system-backup.php
@@ -1,14 +1,95 @@
-<?php
-$folderPath = 'backup';
-
-if (is_dir($folderPath)) {
-    if ($handle = opendir($folderPath)) {
-        while (false !== ($file = readdir($handle))) {
-            if ($file !== "." && $file !== "..") {
-                echo $file . "<br>";
-            }
-        }
-        closedir($handle);
-    }
+<?php include('db_connect.php');?>
+<style>
+	input[type=checkbox]
+{
+  /* Double-sized Checkboxes */
+  -ms-transform: scale(1.3); /* IE */
+  -moz-transform: scale(1.3); /* FF */
+  -webkit-transform: scale(1.3); /* Safari and Chrome */
+  -o-transform: scale(1.3); /* Opera */
+  transform: scale(1.3);
+  padding: 10px;
+  cursor:pointer;
 }
-?>
+</style>
+<div class="container-fluid">
+	
+	<div class="col-lg-12">
+		<div class="row mb-4 mt-4">
+			<div class="col-md-12">
+				
+			</div>
+		</div>
+		<div class="row">
+			<!-- FORM Panel -->
+
+			<!-- Table Panel -->
+			<div class="col-md-12">
+				<div class="card">
+					<div class="card-header">
+						<b>List of Courses and Fees</b>
+						<span class="float:right"><a class="btn btn-danger btn-block btn-sm col-sm-2 float-right" href="javascript:void(0)" id="new_course">
+					<i class="fa fa-plus"></i> New Entry
+				</a></span>
+					
+					</div>
+					<div class="card-body table-responsive">
+						<table class="table table-condensed table-bordered table-hover">
+							<thead>
+								<tr>
+									<th class="text-center">#</th>
+									<th class="">Date</th>
+									<th class="text-center">Action</th>
+								</tr>
+							</thead>
+							<tbody>
+                            <?php
+                                $folderPath = 'backup';
+
+                                if (is_dir($folderPath)) {
+                                    if ($handle = opendir($folderPath)) {
+                                        while (false !== ($file = readdir($handle))) {
+                                            if ($file !== "." && $file !== "..") {
+                                                $modificationTime = date("F d Y H:i:s", filemtime($file));
+                                           
+                                ?>
+								<tr>
+									<td class="text-center"><?php echo $i++ ?></td>
+									<td><?= $modificationTime ?></td>
+									<td class="text-center">
+										<button class="btn btn-sm btn-outline-primary edit_course" type="button" data-id="<?php echo $row['id'] ?>" >Edit</button>
+										<button class="btn btn-sm btn-outline-danger delete_course" type="button" data-id="<?php echo $row['id'] ?>">Delete</button>
+										<button class="btn btn-sm btn-outline-dark view_students_fees" type="button" data-id="<?php echo $row['id'] ?>">Print Students</button>
+									</td>
+								</tr>
+								<?php 
+								 }
+                                }
+                                closedir($handle);
+                            }
+                        }
+                                ?>
+							</tbody>
+						</table>
+					</div>
+				</div>
+			</div>
+			<!-- Table Panel -->
+		</div>
+	</div>	
+
+	
+</div>
+<style>
+	
+	td{
+		vertical-align: middle !important;
+	}
+	td p{
+		margin: unset
+	}
+	img{
+		max-width:100px;
+		max-height: 150px;
+	}
+</style>
