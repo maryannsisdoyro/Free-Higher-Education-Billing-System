9aafa1f5e0f169906a2fcf6d3d2e885d448280e8
maryannsisdoyro <sisdoyromaryannlawan20@gmail.com>
2024-12-22 12:48:11 +0800

    COMMIT

--- enrol/get-sections.php
+++ enrol/get-sections.php
@@ -1,4 +1,6 @@
-<select name="section" class="form-control BSIT" required>
+<?php $course = $row['course'] ?>
+
+<select name="section" class="form-control BSIT <?= $course != 'BSIT' ? 'd-none' : '' ?>" required>
     <!-- <option value="" selected disabled>Select Section</option> -->
     <option value="North">North</option>
     <option value="North East">North East</option>
@@ -8,29 +10,21 @@
     <option value="South East">South East</option>
 </select>
 
-<select name="section" class="form-control BEED d-none" required>
+<select name="section" class="form-control BEED <?= $course != 'BEED' ? 'd-none' : '' ?>" required>
     <!-- <option value="" selected disabled>Select Section</option> -->
     <option value="A">A</option>
     <option value="B">B</option>
     <option value="C">C</option>
 </select>
 
-<select name="section" class="form-control BSED d-none" required>
+<select name="section" class="form-control BSED <?= $course != 'BSED' ? 'd-none' : '' ?>" required>
     <!-- <option value="" selected disabled>Select Section</option> -->
     <option value="A">A</option>
     <option value="B">B</option>
     <option value="C">C</option>
 </select>
 
-<select name="section" class="form-control BSBA d-none" required>
-    <!-- <option value="" selected disabled>Select Section</option> -->
-    <option value="A">A</option>
-    <option value="B">B</option>
-    <option value="C">C</option>
-    <option value="D">D</option>
-</select>
-
-<select name="section" class="form-control BSHM d-none" required>
+<select name="section" class="form-control BSBA <?= $course != 'BSBA' ? 'd-none' : '' ?>" required>
     <!-- <option value="" selected disabled>Select Section</option> -->
     <option value="A">A</option>
     <option value="B">B</option>
@@ -38,5 +32,11 @@
     <option value="D">D</option>
 </select>
 
+<select name="section" class="form-control BSHM <?= $course != 'BSHM' ? 'd-none' : '' ?>" required>
+    <!-- <option value="" selected disabled>Select Section</option> -->
+    <option value="A">A</option>
+    <option value="B">B</option>
+    <option value="C">C</option>
+    <option value="D">D</option>
+</select>
 
-<?php echo $row['course'] ?>
